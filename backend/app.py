bd65a944a9822dec510d54094899a52d656bd910
Prathamesh <prath.parab11@gmail.com>
2026-09-12 06:03:58 +0530

    Connect backend to PostgreSQL

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,7 @@ CORS(
 
 
 # Database configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///orders.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
